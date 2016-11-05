5cd118cf3857c7e8558aa0c54565609d01abce93
fortize <javivi42@hotmail.com>
2016-11-05 19:29:35 +0100

    Modificaciones

---
+++ actividad_colaborativa3.R
@@ -0,0 +1,12 @@
+# Actividad colaborativa módulo 3
+# Autor: Javier Ortiz
+
+#Cargamos las librerias necesarias
+library(gsheet)
+
+#setwd("C:\Users\Javier Ortiz\Desktop\Master Big Data\Modulo 3\M3-Actividad-Colaborativa")
+
+# Bajamos la hoja de cálculo
+url <- 'https://docs.google.com/spreadsheets/d/1CDWBeqpUTBd1TkmDz_M6UGRWdHgU7LOcoiGRTvIttKA/edit#gid=0'
+a <- gsheet2tbl(url)
+
