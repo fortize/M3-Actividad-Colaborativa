acc9a9b37e8d6b5e8820d2d8700338727f15872e
fortize <javivi42@hotmail.com>
2016-11-10 21:20:50 +0100

    10/11/2016

--- actividad_colaborativa3.R
+++ actividad_colaborativa3.R
@@ -3,6 +3,7 @@
 
 #Cargamos las librerias necesarias
 library(gsheet)
+library(xlsx)
 
 #setwd("C:\Users\Javier Ortiz\Desktop\Master Big Data\Modulo 3\M3-Actividad-Colaborativa")
 
@@ -10,6 +11,21 @@ library(gsheet)
 url <- 'https://docs.google.com/spreadsheets/d/1CDWBeqpUTBd1TkmDz_M6UGRWdHgU7LOcoiGRTvIttKA/edit#gid=0'
 datos <- gsheet2tbl(url)
 
-# Ponemos en minúscula todos los datos
-tolower(datos)
+# Preservamos los datos en una hoja de excel
+write.xlsx(datos, "datos.xlsx")
 
+# Modificamos las cabeceras del set de datos
+names(datos[]) 
+
+y <- regexpr("\\.[^\\.]*$", colnames(datos), TRUE)
+z <- y + attr(y, "match.length") - 1
+colnames(datos) <- substr(colnames(datos), 0, z)
+
+# Ponemos en minúscula todos los datos
+tolower(datos[2:5])
+
+# Conversión caracteres html
+tolower(datos[2:5])
+
+# Quitamos caracteres extraños y comas
+tolower(datos[2:5])
